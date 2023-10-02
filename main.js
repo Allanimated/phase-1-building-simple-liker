61a9004a8e779d4423e02db7a41c1db4442024c0
Allan <allcreativewrites@gmail.com>
2023-10-02 19:05:01 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -4,8 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//add class .hidden to error modal
+const modal = document.querySelector('#modal')
+modal.classList.add('hidden')
 
+const hearts = document.querySelectorAll(".like-glyph")
+hearts.forEach(heart => {
+  heart.addEventListener('click', (e) => {
+    if (e.target.innerHTML === FULL_HEART) {
+      e.target.innerHTML = EMPTY_HEART;
+      e.target.classList.remove('activated-heart');
+    } else {
+      handleLike(e);
+    }
+  })
+})
 
+function handleLike(e) {
+  mimicServerCall()
+  .then(resp => {
+    //console.log(resp);
+    changeHeartFill(resp, e)
+  })
+  .catch(error => {
+    modal.innerHTML = error
+  })
+  setTimeout(() => {
+    modal.classList.add('hidden')
+  }, 3000)
+}
+
+function changeHeartFill(resp, e) {
+  if (resp === 'Pretend remote server notified of action!') {
+    e.target.classList.add('activated-heart')
+    e.target.innerHTML = FULL_HEART;
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
